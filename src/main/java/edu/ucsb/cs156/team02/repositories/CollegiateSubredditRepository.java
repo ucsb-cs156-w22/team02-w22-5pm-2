bc3e4d425da3c244eec97996c9532270a1f6cd68
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2022-01-31 13:45:26 -0800

    Update CollegiateSubredditRepository.java

--- src/main/java/edu/ucsb/cs156/team02/repositories/CollegiateSubredditRepository.java
+++ src/main/java/edu/ucsb/cs156/team02/repositories/CollegiateSubredditRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.Todo;
+import edu.ucsb.cs156.example.entities.CollegiateSubredddit;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
