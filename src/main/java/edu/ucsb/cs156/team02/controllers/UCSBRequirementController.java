fa077442fa155fb1a056643330b7b9f22dc969b4
HBzhainan-wzw <59660838+HBzhainan-wzw@users.noreply.github.com>
2022-02-08 15:35:22 -0800

    Edited putMapping

--- src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
@@ -16,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
